1d163611b605c328830ac60b614dc3d9270d8e3f
Sara Abigail Cabascango López <51092317+sacabascango1@users.noreply.github.com>
2022-05-13 16:31:01 -0500

    voice recognition with assistant

---
+++ WinAppSpeechRecognition/WinAppSpeechRecognition/Bot.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
+using System.Diagnostics;
+
+namespace WinAppSpeechRecognition
+{
+    class Bot
+    {
+        // Objeto que representa un conjunto de alternativas de las
+        // restricciones de una gramática de reconocimiento de voz.
+        private Choices mTable;
+        // Objeto que en tiempo de ejecución hace referencia a una 
+        // gramática de reconocimiento de voz, que una aplicación puede
+        // utilizar para definir las restricciones para el reconocimiento
+        // de voz.
+        private Grammar mElements;
+
+        public Choices Table { get => mTable; set => mTable = value; }
+        public Grammar Elements { get => mElements; set => mElements = value; }
+
+        public void InitializeGrammar()
+        {
+            // Inicializa una nueva instancia de la clase Choice que contiene
+            // un conjunto vacío de alternativas.
+            mTable = new Choices();
+            // Se agrega una matriz que contiene uno o más objetos string al
+            // conjunto de alternativas.
+            mTable.Add(new string[] { "hola",
+                                      "prueba",
+                                      "bienvenidos",
+                                      "lenguaje",
+                                      "consonantes",
+                                      "salir",
+                                      "nota",
+                                      "calc" });
+            // Inicializa una nueva instancia de la clase Grammar desde un
+            // objeto GrammarBuilder.
+            mElements = new Grammar(new GrammarBuilder(mTable));
+        }
+
+        public Bot()
+        {
+
+        }
+
+        public void ControlSpeech(SpeechRecognizedEventArgs e, TextBox txtData)
+        {
+            //MessageBox.Show("Speech recognition: " + e.Result.Text.ToString());
+            if (e.Result.Text == "salir")
+            {
+                Application.Exit();
+            }
+            else if (e.Result.Text == "nota")
+            {
+                Process.Start("C:\\Windows\\notepad.exe");
+            }
+            else if (e.Result.Text == "calc")
+            {
+                Process.Start("C:\\Windows\\System32\\calc.exe");
+            }
+            else
+            {
+                txtData.Text = txtData.Text + " " + e.Result.Text.ToString();
+            }
+        }
+
+    }
+}
---
+++ WinAppSpeechRecognition/WinAppSpeechRecognition/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
+using System.Diagnostics;
+
+namespace WinAppSpeechRecognition
+{
+    public partial class frmSpeechRecognition : Form
+    {
+        // Objeto mSynthesizer que permite el acceso a la funcionalidad
+        // de un motor de síntesis de voz instalado (Cortana).
+        private SpeechSynthesizer mSynthesizer = new SpeechSynthesizer();
+        // Objeto mPrompt que permite agregar contenido, seleccionar voces, 
+        // controlar atributos de voz y controlar la pronunciación de las 
+        // palabras dichas.
+        private PromptBuilder mPrompt = new PromptBuilder();
+        // Objeto mRecognition que proporciona los medios para tener acceso
+        // y administrar un motor de reconocimiento de voz en el proceso.
+        private SpeechRecognitionEngine mRecognition = new SpeechRecognitionEngine();
+        // Objeto mBot que maneja la gramática de reconocimiento de voz.
+        private Bot mBot = new Bot();
+
+        public frmSpeechRecognition()
+        {
+            InitializeComponent();
+        }
+
+        private void StartSpeech()
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+
+            mBot.InitializeGrammar();
+
+            try
+            {
+                // Maneja las solicitudes que el reconocedor se detiene para 
+                // actualizar su estado.
+                mRecognition.RequestRecognizerUpdate();
+                // Carga sincrónicamente un obejto de tipo Grammar, utilizando un Bot.
+                mRecognition.LoadGrammar(mBot.Elements);
+                // Se genera cuando el SpeechRegognitionEngine recibe la entrada que 
+                // coincide con cualquiera de sus elementos cargados y habilitados por 
+                // el objeto Grammar.
+                mRecognition.SpeechRecognized += MRecognition_SpeechRecognized; ;
+                // Configura el objeto SpeechRegognitionEngine que va a recibir la entrada
+                // desde el dispositivo de audio predeterminado.
+                mRecognition.SetInputToDefaultAudioDevice();
+                // Realiza una o varias operaciones de reconocimiento de voz asincrónica.
+                mRecognition.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void Speak()
+        {
+            // Borra el contenido del objeto PromptBuilder.
+            mPrompt.ClearContent();
+            // Especifica el texto que se va a anexar al objeto PromptBuilder.
+            mPrompt.AppendText(txtData.Text);
+            // El objeto mSynthesizer llama a la fución Speak() que
+            // permite hablar de forma sincrónica el contenido de un 
+            // objeto PromptBuilder.
+            mSynthesizer.Speak(mPrompt);
+        }
+
+        private void StopSpeech()
+        {
+            // Detiene el reconocimiento asincrónico una vez completada la operación de
+            // reconocimiento actual.
+            mRecognition.RecognizeAsyncStop();
+
+            btnStop.Enabled = false;
+            btnStart.Enabled = true;
+        }
+
+        private void MRecognition_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            mBot.ControlSpeech(e, txtData);
+        }
+
+        private void frmSpeechRecognition_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSpeakText_Click(object sender, EventArgs e)
+        {
+            Speak();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartSpeech();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            StopSpeech();
+        }
+    }
+}
